5954a762e1f7d6e4fb8f019d5ed4675c753724be
amin-dev-tech <amin.dev.tech@gmail.com>
2023-07-26 18:27:43 +0330

    refactoring code

--- common.js
+++ common.js
@@ -4,22 +4,34 @@ const links = document.querySelectorAll('a');
 
 // http request function
 function includeHTML() {
-    let file, xhttp;
-
-    file = main.getAttribute("include-html");
-    if (file) {
-        // Make an http request using the attribute as the file name
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                if (this.status == 200) { main.innerHTML = this.responseText; }
-                if (this.status == 404) { main.innerHTML = "Page not found."; }
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("include-html");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("include-html");
+                    includeHTML();
+                }
             }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
         }
-        xhttp.open("GET", file, true);
-        xhttp.send();
     }
 }
+
+includeHTML();
 
 // apply click event to all links
 // page controllers
