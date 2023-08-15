e7d2213b4ab65c790bb00de13167597569c83735
amin-dev-tech <amin.dev.tech@gmail.com>
2023-08-15 22:43:04 +0330

    weather app

--- weather.js
+++ weather.js
@@ -31,7 +31,14 @@ function handleRepsponse() {
   const resHumidity = document.querySelector(".res-humidity");
   const resWindSpeed = document.querySelector(".res-windspeed");
 
-  if (city === "") return;
+  if (!city) {
+    resCity.innerHTML = `نام شهر : <span class="error">شهر مورد نظر یافت نشد !</span>`;
+    resTemp.innerHTML = "دما : - -";
+    resWeather.innerHTML = "آب و هوا : - -";
+    resHumidity.innerHTML = "رطوبت : - -";
+    resWindSpeed.innerHTML = "سرعت باد : - -";
+    return;
+  }
 
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=${lang}`
@@ -40,84 +47,93 @@ function handleRepsponse() {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
-      resCity.innerHTML = `نام شهر : <span class="response">${data.name}</span>`;
-      resTemp.innerHTML = `دما : <span class="response">${parseInt(
-        data.main.temp
-      )} درجه سانتیگراد </span>`;
-      resWeather.innerHTML = `آب و هوا : <span class="response">${data.weather[0].description}</span>`;
-      resHumidity.innerHTML = `رطوبت : <span class="response">${data.main.humidity}</span>`;
-      resWindSpeed.innerHTML = `سرعت باد : <span class="response">${data.wind.speed}</span>`;
+      if (data.name) {
+        console.log(data);
+        resCity.innerHTML = `نام شهر : <span class="response">${data.name}</span>`;
+        resTemp.innerHTML = `دما : <span class="response">${parseInt(
+          data.main.temp
+        )} درجه سانتیگراد </span>`;
+        resHumidity.innerHTML = `رطوبت : <span class="response">${data.main.humidity}</span>`;
+        resWeather.innerHTML = `آب و هوا : <span class="response">${data.weather[0].description}</span>`;
+        resWindSpeed.innerHTML = `سرعت باد : <span class="response">${data.wind.speed}</span>`;
 
-      responsContainer.style.flex = "2 0 15%";
+        responsContainer.style.flex = "2 0 15%";
 
-      switch (data.weather[0].main) {
-        case "Clouds":
-          img.src = "img/clouds.png";
-          img.animate(
-            [
-              { opacity: 0, transform: "translateY(-8rem)" },
-              { opacity: 1, transform: "translateY(0)" },
-            ],
-            {
-              duration: 1000,
-              easing: "ease-in-out",
-            }
-          );
-          break;
+        switch (data.weather[0].main) {
+          case "Clouds":
+            img.src = "img/clouds.png";
+            img.animate(
+              [
+                { opacity: 0, transform: "translateY(-8rem)" },
+                { opacity: 1, transform: "translateY(0)" },
+              ],
+              {
+                duration: 1000,
+                easing: "ease-in-out",
+              }
+            );
+            break;
 
-        case "Clear":
-          img.src = "img/clear.png";
-          img.animate(
-            [
-              { opacity: 0, transform: "translateY(-8rem)" },
-              { opacity: 1, transform: "translateY(0)" },
-            ],
-            {
-              duration: 1000,
-              easing: "ease-in-out",
-            }
-          );
-          break;
-        case "Rain":
-          img.src = "img/rain.png";
-          img.animate(
-            [
-              { opacity: 0, transform: "translateY(-8rem)" },
-              { opacity: 1, transform: "translateY(0)" },
-            ],
-            {
-              duration: 1000,
-              easing: "ease-in-out",
-            }
-          );
-          break;
-        case "Snow":
-          img.src = "img/snow.png";
-          img.animate(
-            [
-              { opacity: 0, transform: "translateY(-8rem)" },
-              { opacity: 1, transform: "translateY(0)" },
-            ],
-            {
-              duration: 1000,
-              easing: "ease-in-out",
-            }
-          );
-          break;
-        case "Haze":
-          img.src = "img/haze.png";
-          img.animate(
-            [
-              { opacity: 0, transform: "translateY(-8rem)" },
-              { opacity: 1, transform: "translateY(0)" },
-            ],
-            {
-              duration: 1000,
-              easing: "ease-in-out",
-            }
-          );
-          break;
+          case "Clear":
+            img.src = "img/clear.png";
+            img.animate(
+              [
+                { opacity: 0, transform: "translateY(-8rem)" },
+                { opacity: 1, transform: "translateY(0)" },
+              ],
+              {
+                duration: 1000,
+                easing: "ease-in-out",
+              }
+            );
+            break;
+          case "Rain":
+            img.src = "img/rain.png";
+            img.animate(
+              [
+                { opacity: 0, transform: "translateY(-8rem)" },
+                { opacity: 1, transform: "translateY(0)" },
+              ],
+              {
+                duration: 1000,
+                easing: "ease-in-out",
+              }
+            );
+            break;
+          case "Snow":
+            img.src = "img/snow.png";
+            img.animate(
+              [
+                { opacity: 0, transform: "translateY(-8rem)" },
+                { opacity: 1, transform: "translateY(0)" },
+              ],
+              {
+                duration: 1000,
+                easing: "ease-in-out",
+              }
+            );
+            break;
+          case "Haze":
+            img.src = "img/haze.png";
+            img.animate(
+              [
+                { opacity: 0, transform: "translateY(-8rem)" },
+                { opacity: 1, transform: "translateY(0)" },
+              ],
+              {
+                duration: 1000,
+                easing: "ease-in-out",
+              }
+            );
+            break;
+        }
+      } else {
+        resCity.innerHTML = `نام شهر : <span class="error">شهر مورد نظر یافت نشد !</span>`;
+        resTemp.innerHTML = "دما : - -";
+        resWeather.innerHTML = "آب و هوا : - -";
+        resHumidity.innerHTML = "رطوبت : - -";
+        resWindSpeed.innerHTML = "سرعت باد : - -";
+        return;
       }
     });
 }
